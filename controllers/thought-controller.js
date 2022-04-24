6fe0f5113ce015c095889588fb961ec1be067798
HBK <sammcowen@gmail.com>
2022-04-24 16:49:19 -0700

    made some changes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -24,7 +24,7 @@ const thoughtController = {
             select:'-__v'
         })
         .select('-__v')
-        .sort({_id:-1})
+        // .sort({_id:-1})
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
             console.log(err);
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,7 +18,7 @@ const userController = {
     },
     // get user by ID with thoughts
 
-    getUserById(req, res) {
+    getUserById({params}, res) {
         User.findOne({ _id: params.id })
             .populate({
                 path: 'thoughts',
