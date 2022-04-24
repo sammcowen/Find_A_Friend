c1f0ede4803342f268d322f40759cd7b7d4616f5
HBK <sammcowen@gmail.com>
2022-04-23 17:05:17 -0700

    started creating logic inside of controllers folder for user routes

---
+++ controllers/user-controller.js
@@ -0,0 +1,44 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            });
+    },
+    // get user by ID with thoughts
+
+    getUserById(req, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            });
+    },
+
+    // create user
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+}
